aa3a17dfdd4f5504e6c2049f88b03ed6d46184b9
Davide Cuttica <davidefedericocuttica@gmail.com>
2013-06-17 14:36:42 +0200

    First Commit

---
+++ GitExample/src/Example/PushExample.java
@@ -0,0 +1,14 @@
+package Example;
+
+public class PushExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ciao. Come Stai?");
+	}
+
+}
